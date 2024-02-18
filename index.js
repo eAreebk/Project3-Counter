72a24d4edbd208dfb2ae318bd8d5acdbadabf24c
eAreebk <156355720+eAreebk@users.noreply.github.com>
2024-02-18 15:24:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var numb = 1;
+var num = document.getElementById('number');
+num.innerHTML=numb;
+
+document.getElementById("butn1").addEventListener("click", inc);
+document.getElementById("butn2").addEventListener("click", dec);
+document.getElementById("butn3").addEventListener("click", reset);
+document.getElementById("butn4").addEventListener("click", write);
+
+function inc() {
+    numb++; 
+    num.innerHTML=numb;
+}
+   
+function dec() {
+    numb--;
+    num.innerHTML=numb;
+    
+}
+   
+function reset() {
+    numb = 0;
+    num.innerHTML=numb;  
+}
+
+function write() {
+    numb = +prompt("Write Number");
+    num.innerHTML=numb;
+    numb=0;
+}
+
+
+
+
