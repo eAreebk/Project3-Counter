bba85697e250d0c7d10b227a4430bba7c2bc556b
eAreebk <156355720+eAreebk@users.noreply.github.com>
2024-02-18 19:30:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,42 @@ function inc() {
    
 function dec() {
     numb--;
+    if (numb<<0){
+        numb=0;
+    }
+    num.innerHTML=numb;
+}
+   
+function reset() {
+    numb = 0;
+    num.innerHTML=numb;  
+}
+
+function write() {
+    numb = +prompt("Write Number");
+    num.innerHTML=numb;
+    
+}
+
+
+
+
+var numb = 0;
+var num = document.getElementById('number');
+num.innerHTML=numb;
+
+document.getElementById("butn1").addEventListener("click", inc);
+document.getElementById("butn2").addEventListener("click", dec);
+document.getElementById("butn3").addEventListener("click", reset);
+document.getElementById("butn4").addEventListener("click", write);
+
+function inc() {
+    numb++; 
+    num.innerHTML=numb;
+}
+   
+function dec() {
+    numb--;
     if (numb<0){
         numb=0;
     }
